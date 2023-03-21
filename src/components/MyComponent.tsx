0141e9826c74b902eb26b6a028567ff713ce57f0
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-03-21 21:00:23 +0700

    1234

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -10,10 +10,15 @@ export default function MyComponent(props: Props) {
   return (
     <div>
       <hr />
-      <form>
+      <form onSubmit={() => alert("Hey")}>
         <label>{props.label}: </label>
         <br />
-        <input type="text" name="your_message" id="your_message" />
+        <input
+          type="text"
+          name="your_message"
+          id="your_message"
+          onChange={(event) => console.log(event.target.value)}
+        />
         <br />
         <button>{props.btnLabel ? props.btnLabel : "Ok"}</button>
       </form>
