f4ccfecfbceb814a4531f69943d12f2db5b9114f
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-03-21 21:06:24 +0700

    1234

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -7,17 +7,19 @@ type Props = {
 
 // JSX
 export default function MyComponent(props: Props) {
+  let message = "";
+
   return (
     <div>
       <hr />
-      <form onSubmit={() => alert("Hey")}>
+      <form onSubmit={() => alert(message)}>
         <label>{props.label}: </label>
         <br />
         <input
           type="text"
           name="your_message"
           id="your_message"
-          onChange={(event) => console.log(event.target.value)}
+          onChange={(event) => (message = event.target.value)}
         />
         <br />
         <button>{props.btnLabel ? props.btnLabel : "Ok"}</button>
